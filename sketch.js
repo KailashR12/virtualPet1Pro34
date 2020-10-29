ba273f87e5ab6bab8b0ec8c0825fc6a26bcd4866
KailashR12 <69316517+KailashR12@users.noreply.github.com>
2020-10-29 18:53:28 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock
+function preload()
+{
+  //load images here
+  
+
+
+
+
+
+
+}
+
+function setup() {
+	createCanvas(500, 500);
+  dog= new Sprite
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
